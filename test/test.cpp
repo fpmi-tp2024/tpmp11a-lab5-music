8c0db1e02af49a3947a9e441f2dde5defd9fcc6d
Kirill <kirya.budnik@gmail.com>
2024-04-12 22:05:06 +0300

    try number n + 4

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,14 @@
 #include "gtest/gtest.h"
 #include "../include/MusicManager.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../sqlite/sqlite3.h"
 
 TEST(Test, BasicTest)
 {
-    EXPECT_EQ(testFunc1(5), 15);
+    EXPECT_EQ(date_to_number(1, 1, 2000), 946684800);
 }
 
 int main()
