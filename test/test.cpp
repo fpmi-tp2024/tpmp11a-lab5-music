7860ecf414409d9c51b17685ffa7b372af4a7eef
Kirill <kirya.budnik@gmail.com>
2024-04-12 22:39:49 +0300

    Final.

--- test/test.cpp
+++ test/test.cpp
@@ -23,27 +23,27 @@ TEST(DateToNumberTest, February29th2012)
 
 TEST(DateToNumberTest, April15th2015)
 {
-    EXPECT_EQ(date_to_number(15, 4, 2015), 1429046400);
+    EXPECT_EQ(date_to_number(15, 4, 2015), 1429056000);
 }
 
 TEST(DateToNumberTest, June30th2005)
 {
-    EXPECT_EQ(date_to_number(30, 6, 2005), 1120118400);
+    EXPECT_EQ(date_to_number(30, 6, 2005), 1120089600);
 }
 
 TEST(DateToNumberTest, September15th2006)
 {
-    EXPECT_EQ(date_to_number(15, 9, 2006), 1158268800);
+    EXPECT_EQ(date_to_number(15, 9, 2006), 1158278400);
 }
 
 TEST(DateToNumberTest, October20th2007)
 {
-    EXPECT_EQ(date_to_number(20, 10, 2007), 1192848000);
+    EXPECT_EQ(date_to_number(20, 10, 2007), 1192838400);
 }
 
 TEST(DateToNumberTest, November5th2008)
 {
-    EXPECT_EQ(date_to_number(5, 11, 2008), 1225852800);
+    EXPECT_EQ(date_to_number(5, 11, 2008), 1225843200);
 }
 
 TEST(DateToNumberTest, December31st2009)
